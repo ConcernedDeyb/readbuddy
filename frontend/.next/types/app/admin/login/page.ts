792f1e408d4e1fb75d9f64f79f35390b315d78dd
Jayseep <johncliffordepasco@gmail.com>
2026-08-23 23:46:52 +0800

    added admin log in

--- frontend/.next/types/app/admin/login/page.ts
+++ frontend/.next/types/app/admin/login/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\jay\Documents\readbuddy\frontend\app\admin\page.tsx
-import * as entry from '../../../../app/admin/page.js'
+// File: C:\Users\jay\Documents\readbuddy\frontend\app\admin\login\page.tsx
+import * as entry from '../../../../../app/admin/login/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../app/admin/page.js')
+type TEntry = typeof import('../../../../../app/admin/login/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
